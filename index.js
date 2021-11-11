129d7489a8df068663744363bb2578b1344c7385
justinjj2515 <94060752+justinjj2515@users.noreply.github.com>
2021-11-11 09:03:08 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//console.log("HI");
+
+//console.log ('I\'m hello world')
+
+//function sayhello(name){
+    //return ' hello ${name} '
+//}
+
+//console.log(sayhello('mr sharp'))
+
+//console.log('starting task 2')
+
+console.log("hello my name is susan from space")
+
+const name : string  = prompt ("what is your name? : ")
